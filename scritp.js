919a346290e88e9d5c78c1db744dfa8580124851
Lenka Hudčeková <lenkahudcekova@gmail.com>
2023-12-11 18:14:58 +0100

    oprava řešení

--- scritp.js
+++ scritp.js
@@ -10,17 +10,18 @@ Funkce spočítá vaši hrubou měsíční mzdu v celých korunách.
 Dále vytvořte funkci taxed, která na vstupu obdrží částku a procento zdanění, a vrátí částku v celých korunách zdaněnou podle zadaných procent.
 
 Použítím funkcí salary a taxed spočítejte svoji měsíční mzdu po 15% zdanění. */
-
 const salary = (wage, hours, days) => {
-    return document.body.innerHTML += `
-        <p> Vaše hrubá měsíční mzda = ${Math.round(wage*hours*days)} Kč </p>
-    `
+    return Math.round(wage * hours * days)
 }
-salary(150,8,20)
-
-const taxed = (amount, tax) => {
-    return document.body.innerHTML += `
-    <p> Vaše čistá měsíční mzda = ${Math.round(amount*(1-(tax/100)))} Kč </p>
-`
+const input = {
+    wage: Number(prompt("Zadejte hrubou mzdu v Kč za hodinu: ")),
+    hours: Number(prompt("Zadejte kolik hodin průměrně denně pracujete:")),
+    days: Number(prompt("Zadejte kolik dní v měsíci průměrně pracujete:")),
+    amount: Number(prompt("Zadejte výši měsíční hrubé mzdy v Kč: ")),
+    tax: Number(prompt("Zadejte procento zdanění:"))
 }
-taxed(24000,15)
+const taxed = (amount, tax)=> {
+    return Math.round(amount*(1-(tax/100)))    
+}
+document.body.innerHTML += `<p>Vaše hrubá měsíční mzda = ${salary(input.wage, input.hours, input.days)} Kč </p>`
+document.body.innerHTML += `<p> Vaše čistá měsíční mzda = ${taxed(input.amount,input.tax)} Kč</p>`
